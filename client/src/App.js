1f208d195a81d5d6741c05ce3ab72e57f820ef21
Vatsal Vador <vatsal.yogesh.vador@gmail.com>
2021-08-08 02:32:57 -0600

    watch github webhook with pm2

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ class App extends Component {
   callApi = async () => {
     const response = await fetch('/api/hello');
     const body = await response.json();
+    console.log("Checking CD");
     if (response.status !== 200) throw Error(body.message);    return body;
   };
 
